f49221a9b451e218a5a1411571d1ee3034b355bd
Hendri Karisma <hendri.karisma@akarintidata.ai>
2024-07-01 16:52:14 +0700

    from nvd-11-390

--- lua/scala-config.lua
+++ lua/scala-config.lua
@@ -29,33 +29,34 @@ local map = vim.keymap.set
 ----------------------------------
 cmd([[packadd packer.nvim]])
 require("packer").startup(function(use)
-  use({ "wbthomason/packer.nvim", opt = true })
+	use({ "wbthomason/packer.nvim", opt = true })
 
-  use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-vsnip" },
-      { "hrsh7th/vim-vsnip" },
-    },
-  })
-  use({
-    "scalameta/nvim-metals",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "mfussenegger/nvim-dap",
-    },
-  })
 	use({
-	  "nvim-telescope/telescope-packer.nvim",
-	  requires = { 
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim"
+		"hrsh7th/nvim-cmp",
+		requires = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-vsnip" },
+			{ "hrsh7th/vim-vsnip" },
+		},
+	})
+	use({
+		"scalameta/nvim-metals",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"mfussenegger/nvim-dap",
+		},
+	})
+	use({
+		"nvim-telescope/telescope-packer.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim"
 		}
 	})
 
-	use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+	use { "nvim-neotest/nvim-nio" }
 
+	use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
 end)
 
 require("telescope").load_extension "packer"
@@ -67,8 +68,8 @@ require("telescope").load_extension "packer"
 vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
 
 -- LSP mappings
-map("n", "gD",  vim.lsp.buf.definition)
-map("n", "K",  vim.lsp.buf.hover)
+map("n", "gD", vim.lsp.buf.definition)
+map("n", "K", vim.lsp.buf.hover)
 map("n", "gi", vim.lsp.buf.implementation)
 map("n", "gr", vim.lsp.buf.references)
 map("n", "gds", vim.lsp.buf.document_symbol)
@@ -81,38 +82,38 @@ map("n", "<leader>ca", vim.lsp.buf.code_action)
 
 local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 
 -- dap use
 map("n", "<leader>bb", function()
-  dapui.toggle_breakpoint()
+	dapui.toggle_breakpoint()
 end)
 
 map("n", "<leader>bo", function()
-  dapui.open()
+	dapui.open()
 end)
 
 map("n", "<leader>bc", function()
-  dapui.close()
+	dapui.close()
 end)
 
-vim.fn.sign_define("DapBreakpoint", {text = "🛑", texthl = "", linehl = "", numhl = ""})
+vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
 
-vim.fn.sign_define("DapStopped", {text = "🟢", texthl = "", linehl = "", numhl = ""})
+vim.fn.sign_define("DapStopped", { text = "🟢", texthl = "", linehl = "", numhl = "" })
 
 map("n", "<leader>ws", function()
-  require("metals").hover_worksheet()
+	require("metals").hover_worksheet()
 end)
 
 map("n", "<leader>ns", function()
-  require("metals").new_scala_file()
+	require("metals").new_scala_file()
 end)
 
 -- all workspace diagnostics
@@ -120,95 +121,95 @@ map("n", "<leader>aa", vim.diagnostic.setqflist)
 
 -- all workspace errors
 map("n", "<leader>ae", function()
-  vim.diagnostic.setqflist({ severity = "E" })
+	vim.diagnostic.setqflist({ severity = "E" })
 end)
 
 -- all workspace warnings
 map("n", "<leader>aw", function()
-  vim.diagnostic.setqflist({ severity = "W" })
+	vim.diagnostic.setqflist({ severity = "W" })
 end)
 
 -- buffer diagnostics only
 map("n", "<leader>d", vim.diagnostic.setloclist)
 
 map("n", "[c", function()
-  vim.diagnostic.goto_prev({ wrap = false })
+	vim.diagnostic.goto_prev({ wrap = false })
 end)
 
 map("n", "]c", function()
-  vim.diagnostic.goto_next({ wrap = false })
+	vim.diagnostic.goto_next({ wrap = false })
 end)
 
 -- Example mappings for usage with nvim-dap. If you don't use that, you can
 -- skip these
 map("n", "<leader>dc", function()
-  require("dap").continue()
+	require("dap").continue()
 end)
 
 map("n", "<leader>dr", function()
-  require("dap").repl.toggle()
+	require("dap").repl.toggle()
 end)
 
 map("n", "<leader>dK", function()
-  require("dap.ui.widgets").hover()
+	require("dap.ui.widgets").hover()
 end)
 
 map("n", "<leader>dt", function()
-  require("dap").toggle_breakpoint()
+	require("dap").toggle_breakpoint()
 end)
 
 map("n", "<leader>dcl", function()
-  require("dap").clear()
+	require("dap").clear()
 end)
 
 map("n", "<leader>dso", function()
-  require("dap").step_over()
+	require("dap").step_over()
 end)
 
 map("n", "<leader>dsi", function()
-  require("dap").step_into()
+	require("dap").step_into()
 end)
 
 map("n", "<leader>dl", function()
-  require("dap").run_last()
+	require("dap").run_last()
 end)
 
 -- completion related settings
 -- This is similiar to what I use
 local cmp = require("cmp")
 cmp.setup({
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "vsnip" },
-  },
-  snippet = {
-    expand = function(args)
-      -- Comes from vsnip
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- None of this made sense to me when first looking into this since there
-    -- is no vim docs, but you can't have select = true here _unless_ you are
-    -- also using the snippet stuff. So keep in mind that if you remove
-    -- snippets you need to remove this select
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    -- I use tabs... some say you should stick to ins-completion but this is just here as an example
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-  }),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
+	},
+	snippet = {
+		expand = function(args)
+			-- Comes from vsnip
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- None of this made sense to me when first looking into this since there
+		-- is no vim docs, but you can't have select = true here _unless_ you are
+		-- also using the snippet stuff. So keep in mind that if you remove
+		-- snippets you need to remove this select
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		-- I use tabs... some say you should stick to ins-completion but this is just here as an example
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end,
+		["<S-Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end,
+	}),
 })
 
 ----------------------------------
@@ -218,8 +219,8 @@ local metals_config = require("metals").bare_config()
 
 -- Example of settings
 metals_config.settings = {
-  showImplicitArguments = true,
-  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+	showImplicitArguments = true,
+	excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
 
 -- *READ THIS*
@@ -236,38 +237,38 @@ metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 local dap = require("dap")
 
 dap.configurations.scala = {
-  {
-    type = "scala",
-    request = "launch",
-    name = "RunOrTest",
-    metals = {
-      runType = "runOrTestFile",
-      --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-    },
-  },
-  {
-    type = "scala",
-    request = "launch",
-    name = "Test Target",
-    metals = {
-      runType = "testTarget",
-    },
-  },
+	{
+		type = "scala",
+		request = "launch",
+		name = "RunOrTest",
+		metals = {
+			runType = "runOrTestFile",
+			--args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+		},
+	},
+	{
+		type = "scala",
+		request = "launch",
+		name = "Test Target",
+		metals = {
+			runType = "testTarget",
+		},
+	},
 }
 
 metals_config.on_attach = function(client, bufnr)
-  require("metals").setup_dap()
+	require("metals").setup_dap()
 end
 
 -- Autocmd that will actually be in charging of starting the whole thing
 local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
 api.nvim_create_autocmd("FileType", {
-  -- NOTE: You may or may not want java included here. You will need it if you
-  -- want basic Java support but it may also conflict if you are using
-  -- something like nvim-jdtls which also works on a java filetype autocmd.
-  pattern = { "scala", "sbt", "java" },
-  callback = function()
-    require("metals").initialize_or_attach(metals_config)
-  end,
-  group = nvim_metals_group,
+	-- NOTE: You may or may not want java included here. You will need it if you
+	-- want basic Java support but it may also conflict if you are using
+	-- something like nvim-jdtls which also works on a java filetype autocmd.
+	pattern = { "scala", "sbt" },
+	callback = function()
+		require("metals").initialize_or_attach(metals_config)
+	end,
+	group = nvim_metals_group,
 })
